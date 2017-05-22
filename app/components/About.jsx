3af9160fba7d26283ce4ee22a58ef4bb6d889967
Keziyah <keziyahlewis@gmail.com>
2017-05-21 23:05:08 -0400

    didnt get too far but will start again tomorrow by generating stars and then controlling their animation by either css or webgl

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import { Grid, Row, Col } from 'react-bootstrap'
 
-const About = () => {
 
+const About = (props) => {
+    return (
+        <div className="section" id="about">
+            <div className="section-left">
+                <p>{props.text}</p>
+            </div>
+            <div className="container">
+                <Grid>
+                    <Row id="my-photo">
+                        <Col sm={12} className="hori-center">
+                            <img id="myFace" src="/images/myFace.jpg" />
+                        </Col>
+                    </Row>
+
+                    <Row id="text">
+                        <Col sm={12} className="hori-center">
+                            <p id="about-text">I’m a teacher, traveler, and fullstack software engineer based in NYC, specializing in the NERDS (Node.js, Express, React (w/ Redux), Databases using SQL) stack, but always curious to learn more. Currently working on side
+                        projects and looking for a full time engineering gig in New York, DC, or remote.  Feel free to say hi. </p>
+                        </Col>
+                    </Row>
+
+                    <Row className="hori-center" id="logos">
+                        <Col xs={12} sm={6} md={3} >
+                            <i className="fa fa-linkedin fa-5x" aria-hidden="true"></i>
+                            <p className="logo-text">linkedin.com/in/keziyah</p>
+                        </Col>
+
+                        <Col xs={12} sm={6} md={3} >
+                            <i className="fa fa-github fa-5x" aria-hidden="true"></i>
+                            <p className="logo-text">github.com/keziyah</p>
+                        </Col>
+
+                        <Col xs={12} sm={6} md={3} >
+                            <i className="fa fa-twitter fa-5x" aria-hidden="true"></i>
+                            <p className="logo-text">twitter.com/keziyahL</p>
+                        </Col>
+
+                        <Col xs={12} sm={6} md={3} >
+                            <i className="fa fa-envelope fa-5x" aria-hidden="true"></i>
+                            <p className="logo-text">hello@keziyah.com</p>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+            
+        </div>
+    )
 }
 
 export default About; 
--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -3,37 +3,44 @@ import { Grid, Row, Col, Button } from 'react-bootstrap'
 
 const Project = (props) => {
     return (
-        <div className="container section project" id={props.sectionId}>
-            <Grid>
-                <div className="hori-center project-info">
+        <div className="section" id={props.sectionId}>
+            <div id={props.particlesId}>
+            <div className="section-left">
+                <p>{props.text}</p>
+            </div>
+            <div className="container project">
+                <Grid>
+                    <div className="hori-center project-info">
 
-                    <Row>
-                        <Col sm={12}>
-                            <h1 className="project-name" id={props.headerId}>{props.name}</h1>
+                        <Row>
+                            <Col sm={12}>
+                                <h1 className="project-name" id={props.headerId}>{props.name}</h1>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col sm={12}>
+                                <h1 className="project-tag">{props.tagline}</h1>
+                            </Col>
+                        </Row>
+
+                        <Row className="my-button">
+                            <Col sm={12}>
+                                <Button>Learn More</Button>
+                            </Col>
+                        </Row>
+
+                    </div>
+
+                    <Row className="project-down">
+                        <Col sm={12} className="hori-center">
+                            <i className="fa fa-chevron-down fa-5x" aria-hidden="true"></i>
                         </Col>
                     </Row>
 
-                    <Row>
-                        <Col sm={12}>
-                            <h1 className="project-tag">{props.tagline}</h1>
-                        </Col>
-                    </Row>
-
-                    <Row className="my-button">
-                        <Col sm={12}>
-                            <Button>Learn More</Button>
-                        </Col>
-                    </Row>
-
-                </div>
-
-                <Row className="project-down">
-                    <Col sm={12} className="hori-center">
-                        <i className="fa fa-chevron-down fa-5x" aria-hidden="true"></i>
-                    </Col>
-                </Row>
-
-            </Grid>
+                </Grid>
+            </div>
+            </div>
         </div>
     )
 }
