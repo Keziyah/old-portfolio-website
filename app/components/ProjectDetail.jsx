47dfb1703cece2eddf07492fb6ed8d6a5b4a080b
Keziyah <keziyahlewis@gmail.com>
2017-05-22 10:58:50 -0400

    just starting

---
+++ app/components/ProjectDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Grid, Row, Col, Button } from 'react-bootstrap'; 
+
+const ProjectDetail = (props) => {
+    return (
+        <div>Hello i am project detail</div>
+    )
+}
+
+export default ProjectDetail
