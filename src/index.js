3177897656455e17a839ce9903fbef01da95e0b8
Keziyah <keziyahlewis@gmail.com>
2017-12-16 17:19:21 -0500

    new design starting point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga'
+//import ReactGA from 'react-ga'
 import {
   BrowserRouter as Router,
   Route, Switch
@@ -18,24 +18,23 @@ import NoMatch from './components/NoMatch'
 import MyStory from './components/MyStory'
 
 
-import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import rootReducer from './redux/index.js'
+// import { Provider } from 'react-redux'
+// import { applyMiddleware, createStore } from 'redux'
+// import { createLogger } from 'redux-logger'
+// import thunk from 'redux-thunk'
+// import rootReducer from './redux/index.js'
 
-const middleware = applyMiddleware(thunk, createLogger());
-export const store = createStore(rootReducer, middleware);
+// const middleware = applyMiddleware(thunk, createLogger());
+// export const store = createStore(rootReducer, middleware);
 
-ReactGA.initialize('UA-53642101-2')
-function logPageView() {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
+// ReactGA.initialize('UA-53642101-2')
+// function logPageView() {
+//   ReactGA.set({ page: window.location.pathname + window.location.search });
+//   ReactGA.pageview(window.location.pathname + window.location.search);
+// }
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router onUpdate={logPageView} >
+        <Router>
             <div>
                 <Switch>
                     <Route exact path="/" component={App} />
@@ -49,8 +48,7 @@ ReactDOM.render(
                     <Route component={NoMatch} />
                 </Switch>
             </div>
-        </Router>
-    </Provider>,
+        </Router>,
      document.getElementById('root'));
 
 registerServiceWorker();
