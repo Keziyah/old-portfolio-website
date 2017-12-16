7ff3d86863ce507111d0317939fc1ad6b3181828
Keziyah <keziyahlewis@gmail.com>
2017-12-16 17:26:59 -0500

    new redesign starting point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import ReactGA from 'react-ga'
 import {
   BrowserRouter as Router,
   Route, Switch
@@ -17,21 +16,6 @@ import Kursor from './components/Kursor'
 import NoMatch from './components/NoMatch'
 import MyStory from './components/MyStory'
 
-
-// import { Provider } from 'react-redux'
-// import { applyMiddleware, createStore } from 'redux'
-// import { createLogger } from 'redux-logger'
-// import thunk from 'redux-thunk'
-// import rootReducer from './redux/index.js'
-
-// const middleware = applyMiddleware(thunk, createLogger());
-// export const store = createStore(rootReducer, middleware);
-
-// ReactGA.initialize('UA-53642101-2')
-// function logPageView() {
-//   ReactGA.set({ page: window.location.pathname + window.location.search });
-//   ReactGA.pageview(window.location.pathname + window.location.search);
-// }
 
 ReactDOM.render(
         <Router>
